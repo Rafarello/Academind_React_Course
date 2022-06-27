aab4c4b7550ee0cbe682ce6f82f871a888b518f0
Rafael Moraes <rafael1moraes1rocha@gmail.com>
2022-06-27 18:00:08 -0300

    Feat: Componente Expenses

--- Module1_Basics_Components/class-1-basics/src/components/Expenses/Expenses.js
+++ Module1_Basics_Components/class-1-basics/src/components/Expenses/Expenses.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './styles/Expenses.css';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from '../NewExpense/ExpensesFilter';
 
-function Expenses({expensesArray}) {
+function Expenses({expensesArray, onFilterChange}) {
   return (
     <div className="expenses">
+      <ExpensesFilter
+        onFilterChange={onFilterChange}
+      />
       {expensesArray.map((expense) => (
         <ExpenseItem
           key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
+          onFilterChange={onFilterChange}
         />
       ))}
     </div>
